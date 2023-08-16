df4aed04e0cb63e36a4b8901710a5dd6575a044a
stephendambo <dambolin9@gmail.com>
2023-08-16 16:59:37 -0700

    Services page

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+// For Navbar
 let navbar = document.querySelector('.navbar');
+
 document.querySelector('#Menu').onclick = () => {
     navbar.classList.toggle('active');
 }
+
+// For Country Code
+function country_code () {
+    var val = document.getElementById("country").value;
+
+    if (val === "select country") {
+      document.getElementById("output").value = "";
+    }
+
+    else if (val === "tz"){
+      document.getElementById("output").value = "+255";
+    }
+      
+    else if (val === "za"){
+      document.getElementById("output").value = "+27";
+    }
+  
+    else if (val === "zm"){
+        document.getElementById("output").value = "+260";
+    }
+
+    else if (val === "gh"){
+        document.getElementById("output").value = "+233";
+    }
+
+    else if (val === "ng"){
+      document.getElementById("output").value = "+234";
+    }
+   }
+
+ 
